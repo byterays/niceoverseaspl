c2e0259195682696f73e152776c9f976ad11477d
Alien Shrestha <npalien4u@gmail.com>
2026-02-07 12:43:29 +0000

    main site logo and man-menu fixed

--- platform/themes/niceoverseas/partials/header.blade.php
+++ platform/themes/niceoverseas/partials/header.blade.php
@@ -70,44 +70,18 @@
 			<nav class="navbar navbar-expand-lg">
 				<div class="container">
 					<!-- Logo Start -->
-					<a class="navbar-brand" href="index.html">
-						<img src="images/logo.svg" alt="Logo">
+					<a class="navbar-brand" href="/">						
+                        <img src="{{ RvMedia::getImageUrl(theme_option('logo')) }}" width="250">
 					</a>
 					<!-- Logo End -->
 
 					<!-- Main Menu Start -->
 					<div class="collapse navbar-collapse main-menu">
                         <div class="nav-menu-wrapper">
-                            <ul class="navbar-nav mr-auto" id="menu">
-                                <li class="nav-item submenu"><a class="nav-link" href="index.html">Home</a>
-                                    <ul>
-                                        <li class="nav-item"><a class="nav-link" href="index-2.html">Home - Main</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="index-image.html">Home - Image</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="index-video.html">Home - Video</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="index-slider.html">Home - Slider</a></li>
-                                    </ul>
-                                </li>                                
-                                <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
-                                <li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
-                                <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                                <li class="nav-item submenu"><a class="nav-link" href="#">Pages</a>
-                                    <ul>                                        
-                                        <li class="nav-item"><a class="nav-link" href="service-single.html">Service Details</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="project-single.html">Project Details</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="team.html">Team</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="team-single.html">Team Details</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="pricing.html">Pricing Plan</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="testimonials.html">Testimonials</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="image-gallery.html">Image Gallery</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="video-gallery.html">Video Gallery</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="faqs.html">FAQs</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="404.html">404</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
-                            </ul>
+                            {!! Menu::renderMenuLocation('main-menu', [
+                                'view' => 'main-menu',
+                                'options'=>['class'=>"navbar-nav mr-auto", "id"=>"menu"]
+                            ]) !!}
                         </div>
 
                         <!-- Header Btn Start -->
--- platform/themes/niceoverseas/partials/main-menu.blade.php
+++ platform/themes/niceoverseas/partials/main-menu.blade.php
@@ -1,14 +1,14 @@
-<ul {!! BaseHelper::clean($options) !!}>
+<ul  {!! BaseHelper::clean($options) !!}>
     @foreach ($menu_nodes as $key => $row)
-    <li class="@if($row->has_child) dropdown @endif menu-item @if ($row->has_child) menu-item-has-children @endif {{ $row->css_class }} @if ($row->active) active @endif">
-        <a href="{{ $row->url }}" target="{{ $row->target }}">{{ $row->title }}</a>
+    <li class=" nav-item @if($row->has_child) submenu @endif {{ $row->css_class }} @if ($row->active) active @endif">
+        <a href="{{ $row->url }}" target="{{ $row->target }}" class="nav-link" >{{ $row->title }}</a>
             @if ($row->has_child)
                 {!!
                     Menu::generateMenu([
                         'menu' => $menu,
                         'menu_nodes' => $row->child,
                         'view' => 'main-menu',
-                        'options' => ['class' => 'sub-menu'],
+                        'options' => ['class' => ''],
                     ])
                 !!}
             @endif
