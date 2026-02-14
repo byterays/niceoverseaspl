5978f2bcf5ec0028c20cacc0f2b9c192ba62af80
Alien Shrestha <npalien4u@gmail.com>
2026-02-14 17:02:47 +0000

    added Widget classes

---
+++ platform/themes/niceoverseas/widgets/ServiceCategoriesWidget.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Theme\Niceoverseas\Widgets;
+
+use Botble\Base\Forms\FormAbstract;
+use Botble\Widget\AbstractWidget;
+
+class ServiceCategoriesWidget extends AbstractWidget
+{
+    public function __construct()
+    {
+        parent::__construct([
+            'id' => 'service-categories',
+            'name' => 'Service Categories',
+            'description' => 'Service navigation widget',
+        ]);
+    }
+
+    public function settingForm(): FormAbstract|string|null
+    {
+        return $this->formBuilder->createByArray([
+            'title' => [
+                'type' => 'text',
+                'label' => 'Widget Title',
+            ],
+        ]);
+    }
+
+    public function run()
+    {
+        return view(
+            Theme::getThemeNamespace() . '::partials.widgets.service-categories',
+            ['settings' => $this->getConfig()]
+        );
+    }
+}
---
+++ platform/themes/niceoverseas/widgets/SidebarCtaWidget.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Theme\Niceoverseas\Widgets;
+
+use Botble\Base\Forms\FormAbstract;
+use Botble\Widget\AbstractWidget;
+
+class SidebarCtaWidget extends AbstractWidget
+{
+    public function __construct()
+    {
+        parent::__construct([
+            'id' => 'sidebar-cta',
+            'name' => 'Sidebar CTA',
+            'description' => 'Sidebar CTA widget',
+        ]);
+    }
+
+    public function settingForm(): FormAbstract|string|null
+    {
+        return $this->formBuilder->createByArray([
+            'heading' => [
+                'type' => 'text',
+                'label' => 'Heading',
+            ],
+            'phone' => [
+                'type' => 'text',
+                'label' => 'Phone',
+            ],
+            'logo' => [
+                'type' => 'mediaImage',
+                'label' => 'Logo Image',
+            ],
+            'phone_icon' => [
+                'type' => 'mediaImage',
+                'label' => 'Phone Icon',
+            ],
+            'animation_delay' => [
+                'type' => 'text',
+                'label' => 'Animation Delay',
+                'default_value' => '0.25s',
+            ],
+        ]);
+    }
+
+    public function run()
+    {
+        return view(
+            Theme::getThemeNamespace() . '::partials.widgets.sidebar-cta',
+            ['settings' => $this->getConfig()]
+        );
+    }
+}
